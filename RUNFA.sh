2b68fc7f7f357b1a3db5e0d022cdcbe72b810c3a
hhhvmmkfxssaa <88067433+hhhvmmkfxssaa@users.noreply.github.com>
2021-09-19 23:16:43 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -63,7 +63,7 @@ echo -e "\e[38;5;77m       | |_ / _ \ |  _| | | | |  _| | |_) |\e[0m"
 echo -e "\e[38;5;77m       |  _/ _ \| |_| |_| | |_|  _ <\e[0m"
 echo -e "\e[38;5;77m       |_|/_/   \_\_|/|_|_| \_\\e[0m"
 
-echo -e "\e[38;5;77m       •{ Faeder source has been installed }•\e[0m"
+echo -e "\e[38;5;77m       •{ angel source has been installed }•\e[0m"
 echo -e "\e[38;5;77m       •{ Source developer : @TTITITl }•\e[0m"
 cd ..
 rm -rf luarocks*
